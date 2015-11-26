13bb235a5afbc4791d18f501127c680b6b626d10
faucets <madmat.blc@gmail.com>
2015-11-26 23:16:23 +0100

    Update faucets list

--- g.js
+++ g.js
@@ -1,6 +1,16 @@
 var faucets = [
-    'http://milli.io/?r=',
-    'http://bibifaucet.com/?r='
+    'http://milli.io/?r=', //65%
+    'http://www.siizo.com/?r=', //40%
+    'https://f3-btc.com/?r=', //30%
+    'http://itsrainingbitcoins.com/?r=', //20%
+    'http://satoshifiesta.com/?r=', //15%
+    'http://bibifaucet.com/?r=', //10%
+    'http://razercrypt.com/btc/?r=', //?
+    'http://btc4you.net/?r=', //10%
+    'http://bluesatoshi.com/?r=', // 10%
+    'http://dorifaucet.com/btc/?r=', //10%
+    'http://www.free-faucet.eu/?r=', //10%
+    'http://bitcoindrips.com/?r=', //10%
 ];
 
 var refAddress = '1AjgZS5n498VP5cEokGe7WWdz1ijS4pxP8';
