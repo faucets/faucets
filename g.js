a0a14e13888d534dc70cd4c5ed833febe7f26ae9
Mathieu Gorichon <mathieu.gorichon@gmail.com>
2015-12-07 21:32:01 +0100

    Update faucets

--- g.js
+++ g.js
@@ -1,19 +1,27 @@
+// TODO
+// - Add refresh button (usefull for adblock)
+
 var faucets = [
     'http://milli.io/?r=', //65%
-    'http://www.siizo.com/?r=', //40%
-    'https://f3-btc.com/?r=', //30%
-    'http://itsrainingbitcoins.com/?r=', //20%
-    'http://satoshifiesta.com/?r=', //15%
-    'http://bibifaucet.com/?r=', //10%
-    'http://razercrypt.com/btc/?r=', //?
-    'http://btc4you.net/?r=', //10%
-    'http://bluesatoshi.com/?r=', // 10%
-    'http://dorifaucet.com/btc/?r=', //10%
-    'http://www.free-faucet.eu/?r=', //10%
-    'http://bitcoindrips.com/?r=', //10%
+    'https://f3-btc.com/?r=', //30% - 60minutes // referral ok
+    'http://itsrainingbitcoins.com/?r=', //20% - 500S - 900 minutes // referral broken ?
+    'http://razercrypt.com/btc/?r=', //? // referral broken ?
+    'http://bluesatoshi.com/?r=', // 10% // referral broken ?
+    'http://dorifaucet.com/btc/?r=', //10% // referral broken ?
+    'http://www.free-faucet.eu/?r=', //10% // referral broken ?
+    'http://bitcoindrips.com/?r=', //10%// referral broken ?
+    'http://satoshi.ws/?r=', // referral ok
+    'http://www.satoshididu.com/?r=',  // referral broken ?
 ];
 
-var refAddress = '1AjgZS5n498VP5cEokGe7WWdz1ijS4pxP8';
+var faucets2 = [
+    {name: 'milli.io', url: 'http://milli.io/?r=', period: '60'},
+    {name: 'f3-btc.com', url: 'https://f3-btc.com/?r=', period: '60'},
+    {name: 'satoshi.ws', url: 'http://satoshi.ws/?r=', period: '400'},
+];
+
+var address = '1AjgZS5n498VP5cEokGe7WWdz1ijS4pxP8';
+var refAddress = '1A5NFjjFrHc7JFKTxHN7NeHm7tHPdQjtax';
 
 var index = 0;
 
